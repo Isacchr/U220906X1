953894bc23306a34a7655ebf2d6fd66500cec824
isacchr <isac.christiansson@gmail.com>
2022-09-06 12:51:43 +0200

    added two div through js and h1 in both elements

--- public/app.js
+++ public/app.js
@@ -5,10 +5,27 @@ function build() {
 
     //console.log('Hey from javascript');
 
-    const target = document.getElementById('app');
-    const newElement = document.createElement('p');
-    newElement.innerHTML = 'TESTING';
-    newElement.className = 'red';
-    target.appendChild(newElement);
+    const targetSettings = document.getElementById('app');
+    const divSettings = document.createElement('div');
+    divSettings.setAttribute('id', 'settings');
+    targetSettings.appendChild(divSettings);
+    
+    const targetDivSettings = document.getElementById('settings');
+    const titleSettings = document.createElement('h1');
+    titleSettings.innerHTML = 'Settings';
+    titleSettings.className = 'border';
+    targetDivSettings.appendChild(titleSettings);
+    
+
+    const targetDivCalc = document.getElementById('app');
+    const divCalc = document.createElement('div');
+    divCalc.setAttribute('id', 'calculator');
+    targetDivCalc.appendChild(divCalc);
+
+    const targetCalc = document.getElementById('calculator');
+    const titleCalc = document.createElement('h1');
+    titleCalc.innerHTML = 'Calculator';
+    titleCalc.className = 'border';
+    targetCalc.appendChild(titleCalc);
 
 }
