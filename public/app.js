6d3800998406da47e58445bf6d19374dcca6afbb
isacchr <isac.christiansson@gmail.com>
2022-09-06 12:19:22 +0200

    used javacript to add elements in HTLM

--- public/app.js
+++ public/app.js
@@ -1,1 +1,14 @@
-console.log('Hey from js');
+
+build();
+
+function build() {
+
+    //console.log('Hey from javascript');
+
+    const target = document.getElementById('app');
+    const newElement = document.createElement('p');
+    newElement.innerHTML = 'TESTING';
+    newElement.className = 'red';
+    target.appendChild(newElement);
+
+}
