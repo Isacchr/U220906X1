61353af682346dc009966d9dc3829b2e28f21c92
isacchr <isac.christiansson@gmail.com>
2022-09-06 13:47:52 +0200

    refactored js code, excessive code deleted

--- public/app.js
+++ public/app.js
@@ -1,56 +1,52 @@
-
 build();
 
-calculator();
+
 
 function build() {
 
     //console.log('Hey from javascript');
 
-    const targetSettings = document.getElementById('app');
     const divSettings = document.createElement('div');
     divSettings.setAttribute('id', 'settings');
-    targetSettings.appendChild(divSettings);
+    app.appendChild(divSettings);
     
-    const targetDivSettings = document.getElementById('settings');
     const titleSettings = document.createElement('h1');
     titleSettings.innerHTML = 'Settings';
-    targetDivSettings.appendChild(titleSettings);
+    divSettings.appendChild(titleSettings);
     
-
-    const targetDivCalc = document.getElementById('app');
     const divCalc = document.createElement('div');
     divCalc.setAttribute('id', 'calculator');
-    targetDivCalc.appendChild(divCalc);
+    app.appendChild(divCalc);
 
-    const targetCalc = document.getElementById('calculator');
     const titleCalc = document.createElement('h1');
     titleCalc.innerHTML = 'Calculator';
-    targetCalc.appendChild(titleCalc);
+    divCalc.appendChild(titleCalc);
+
+    calculatorWindow(divCalc);
+    
+}
+
+
+function settingsWindow() {
+
+
 
 }
 
 
-function settings() {
+function calculatorWindow(divCalc) {
 
-
-
-}
-
-
-function calculator() {
-
-    const targetNumberInput1 = document.getElementById('calculator');
     const numberInput1 = document.createElement('input');
     numberInput1.setAttribute('id', 'input1');
-    targetNumberInput1.appendChild(numberInput1);
+    divCalc.appendChild(numberInput1);
 
-    const targetNumberInput2 = document.getElementById('calculator');
     const numberInput2 = document.createElement('input');
     numberInput2.setAttribute('id', 'input2');
-    targetNumberInput2.appendChild(numberInput2);
+    divCalc.appendChild(numberInput2);
 
-    const buttonAddition = document.getElementById('calculator');
+
+    const buttonAddition = document.getElementById()
+
     
 
 }
